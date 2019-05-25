7060b0883e6d39719a5bda527d70eb695df1dbdc
Reza Mohebbi <rezaqom1999@gmail.com>
2019-05-25 14:28:12 +0430

    =start project

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <div id="app">
+    <app-head></app-head>
+
+
+    <app-body></app-body>
+
+
+    <app-footer></app-footer>
+  </div>
+
+
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Body from './components/Body.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    appHead: Header,
+    appBody: Body,
+    appFooter: Footer,
+  },
+}
+</script>
+
+<style>
+
+</style>
